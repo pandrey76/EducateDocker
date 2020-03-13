c288d517a876126029759bd996d86c3679ae5934
pandrey76 <pandrey76@yandex.ru>
2020-03-13 16:32:42 +0300

    Docker Basics Big Practical Release
    Running Mongo. Continue of video lesson (YOUTUBE User: Artyom Matyashov) (00:49:04).

--- Docker-mongo-example/app.py
+++ Docker-mongo-example/app.py
@@ -1,21 +1,25 @@
-import time
 from uuid import uuid4
-from . storage import MongodbService
+import importlib
+module_name = 'storage'
+my_module = importlib.import_module(module_name)
 
 # Создаётся объект класса MongodbService,
 # у этого класса есть всего два метода get_data, save_data.
-storage = MongodbService.get_instance()
+# Почему, то данный код не работает
+# storage1 = my_module.MongodbService.get_instance()
+
+
+storage1 = my_module.MongodbService()
 
 #  В цикле записываются 5 объектов, объекты простые у них есть два поля.
 # Туда записывается UUID версии 4.
-
 for _ in range(5):
     dto = {
         "_id": str(uuid4()),
         "payload": str(uuid4())
     }
-    storage.save_data(dto)
+    storage1.save_data(dto)
 
-# Выводится вся информация, кои=торая там есть
-for data in storage.get_data():
+# Выводится вся информация, которая там есть
+for data in storage1.get_data():
     print(data)
